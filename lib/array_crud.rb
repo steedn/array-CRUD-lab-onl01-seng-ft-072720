dcd21824f336811c5ad8f76f810d3e7496821077
Nathan Steed <steedn@outlook.com>
2020-07-28 18:51:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,22 +3,26 @@ def create_an_empty_array
 end
 
 def create_an_array
-["hello", 43, 5, 7]
+hello = [45,46,47,48]
 end
 
 def add_element_to_end_of_array(array, element)
-
+array = [35,46,57]
+array << element
 end
 
 def add_element_to_start_of_array(array, element)
-
+array = [ 490,47,35,14]
+array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
+array = [15,37,13]
 
 end
 
 def remove_element_from_start_of_array(array)
+array = [43,5,6,8]
 
 end
 
